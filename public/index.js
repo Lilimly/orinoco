fe8e999b0411fa031961f6e00d5e81ef76de50a6
Aurélie Mlynarz <aureliemlynarz@gmail.com>
2020-07-28 18:12:17 +0200

    Définition fonction getTeddies

--- public/index.js
+++ public/index.js
@@ -1,7 +1,51 @@
-let jsondata = fetch("http://localhost:3000/api/teddies")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => alert("Erreur : " + error));
+const element = document.getElementById('teddies');
+element.innerHTML = '<section class="teddy"><img class="teddy_img" src=""><h3 class="teddy_name"></h3></section>';
+const teddySection = element.innerHTML;
+
+const getTeddies = async function() {
+    let response = await fetch('http://localhost:3000/api/teddies')
+    let teddies = await response.json()
+    console.log(teddies);
+
+    for (let teddy of teddies) {
+        console.log(teddy);
+
+        const newTeddy = document.createRange().createContextualFragment(teddySection);
+        
+        newTeddy.querySelector('.teddy_img').src = teddy.imageUrl;
+        newTeddy.querySelector('.teddy_name').textContent = teddy.name;
+        
+        element.appendChild(newTeddy);
+    }
+    
+
+}
+
+
+getTeddies();
+
+
+
+
+
+/* function getTeddies(){
+    const response = fetch("http://localhost:3000/api/teddies")
+        .then(response => response.json()).then(console.log)
+        .catch(error => alert("Erreur : " + error));
+
+    console.log("response = " + response);
+    
+    for(i = 0; i < teddies.length; i++){
+        console.log(teddies[i]);
+    }
+}
+
+getTeddies(); */
+
+
+
+
+
 
 
 
